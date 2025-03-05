7fde966ecfd3ef51f9ccfda2945f6fbf50bf69a8
unknown <odeyemipreciousayomikun@yahoo.com>
2025-03-05 13:50:48 +0100

    latest update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleFAQ(button) {
     var answer = button.nextElementSibling;
     answer.classList.toggle("show");
   }
-  const btnHdE1 = document.getElementById('btnHd"');
+  const btnHdE1 = document.getElementById('btnHd');
   btnHd.addEventListener("click", function () {
     window.location.href = "createAccount.html";
   });
